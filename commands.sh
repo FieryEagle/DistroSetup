4e6b53d7bae7755f1621a48d4e77a629523d3cdd
FieryEagle <mihitmittal@gmail.com>
2020-07-14 14:50:17 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -15,7 +15,7 @@ sudo python3 setup.py install
 
 #Copy over config file and set libinput to start
 
-cp -rf libinput-gestures.conf ./config/
+mv ~/libinput-gestures.conf ~/.config/
 ibinput-gestures-setup autostart
 libinput-gestures-setup start
 cd ~/
@@ -24,13 +24,16 @@ cd ~/
 
 sudo apt-get purge firefox* -y
 sudo apt install neofetch ruby python3-pip stacer apt-transport-https curl ffmpegthumbnailer vlc dmz-cursor-theme discord gnome-tweaks qbittorrent speedtest-cli gstreamer1.0-libav -y
-sudo gem install lolcat
+sudo gem install lolcat -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 wget https://launcher.mojang.com/download/Minecraft.deb
 sudo apt install ~/google-chrome-stable_current_amd64.deb -y
 sudo apt install ~/teamviewer_amd64.deb -y
 sudo apt install ~/Minecraft.deb -y
+rm -rf ~/Minecraft.deb
+rm -rf ~/teamviewer_amd64.deb
+rm -rf ~/google-chrome-stable_current_amd64.deb
 
 #Install Wine
 
@@ -49,6 +52,7 @@ sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/bobafetthotmail/ref
 
 #Installs fonts
 
+mv ~/DistroSetup/.fonts ~/
 fc-cache -rv
 
 #Janitor work
