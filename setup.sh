e2cdf08453b10c9cbb708f68abced0cdd4c5fab2
FieryEagle <mihitmittal@gmail.com>
2020-10-02 18:05:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,6 +45,7 @@ sudo apt-get install gstreamer1.0-libav -y
 sudo apt-get install htop -y
 sudo apt install ratbagd -y
 sudo apt install piper -y
+sudo apt install telegram-desktop -y
 wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
 sudo apt-get install atom -y
